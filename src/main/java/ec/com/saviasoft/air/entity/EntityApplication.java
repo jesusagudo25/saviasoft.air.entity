55bcabaeafdb2997dd2168d2483aee6389138bad
Jesus Agudo <jagudo2514@gmail.com>
2023-12-11 21:51:43 -0500

    Default device for example

--- src/main/java/ec/com/saviasoft/air/entity/EntityApplication.java
+++ src/main/java/ec/com/saviasoft/air/entity/EntityApplication.java
@@ -1,7 +1,11 @@
 package ec.com.saviasoft.air.entity;
 
+import ec.com.saviasoft.air.entity.model.request.DeviceRequest;
+import ec.com.saviasoft.air.entity.service.DeviceService;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class EntityApplication {
@@ -10,4 +14,20 @@ public class EntityApplication {
 		SpringApplication.run(EntityApplication.class, args);
 	}
 
+	@Bean
+	public CommandLineRunner commandLineRunner(
+			DeviceService service
+	) {
+		return args -> {
+			var device = service.createDevice(DeviceRequest.builder()
+					.userId(1)
+					.name("Device 1")
+					.serialNumber("123456789")
+					.location("Quito")
+					.uid("b3ff7c1e-85d2-11ee-b9d1-0242ac120002")
+					.status(true)
+					.build());
+			System.out.println("Device: " + device);
+		};
+	}
 }
--- src/main/java/ec/com/saviasoft/air/entity/service/DeviceService.java
+++ src/main/java/ec/com/saviasoft/air/entity/service/DeviceService.java
@@ -2,8 +2,11 @@ package ec.com.saviasoft.air.entity.service;
 
 import ec.com.saviasoft.air.entity.data.DeviceRepository;
 import ec.com.saviasoft.air.entity.model.pojo.Device;
+import ec.com.saviasoft.air.entity.model.request.DeviceRequest;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+
+import java.util.Date;
 
 @Service
 @RequiredArgsConstructor
@@ -14,6 +17,23 @@ public class DeviceService {
     public Device findByUid(String uid) {
         try {
             return repository.findByUid(uid);
+        } catch (Exception e) {
+            return null;
+        }
+    }
+
+    public Device createDevice(DeviceRequest deviceRequest) {
+        try {
+            return repository.save(Device.builder()
+                    .userId(deviceRequest.getUserId())
+                    .name(deviceRequest.getName())
+                    .serialNumber(deviceRequest.getSerialNumber())
+                    .location(deviceRequest.getLocation())
+                    .uid(deviceRequest.getUid())
+                    .status(deviceRequest.getStatus())
+                    .createdDate(new Date())
+                    .updatedDate(new Date())
+                    .build());
         } catch (Exception e) {
             return null;
         }
